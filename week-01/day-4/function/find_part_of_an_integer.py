d7ad94d017662a3dd54881db7d54ee768db3604d
angela <Angela_Shi@epam.com>
2019-05-10 15:02:47 +0800

    update find_part_of_an_integer.py

--- week-01/day-4/function/find_part_of_an_integer.py
+++ week-01/day-4/function/find_part_of_an_integer.py
@@ -2,13 +2,18 @@
 #  Returns the indeces of the numbers in the list where the first number is part of
 #  Or returns an empty list if the number is not part of any of the numbers in the list
 # Example
-print(subint(1, [1, 11, 34, 52, 61]))
+#print(subint(1, [1, 11, 34, 52, 61]))
 # should print: `[0, 1, 4]`
-print(subint(9, [1, 11, 34, 52, 61]))
+#print(subint(9, [1, 11, 34, 52, 61]))
 # should print: '[]'
 
 def subint(num,target_list):
-    for i in range(len(target_list))
+    output=[]
+    for i in range(len(target_list)):
+        if str(num) in str(target_list[i]):
+            find_num=int(str(target_list[i]))
+            output.append(find_num)
+    return output
 
-subint(1,[12,45,15])
+print(subint(1,[12,45,15]))
 
