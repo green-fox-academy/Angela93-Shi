5d1368ab61ae9f1a8b05890860c0563dcc097320
angela <Angela_Shi@epam.com>
2019-05-13 23:02:26 +0800

    update mentor.py

--- week-02/day-7/inheritance/mentor.py
+++ week-02/day-7/inheritance/mentor.py
@@ -1,17 +1,19 @@
 from person import Person
 
 class Mentor(Person):
-    def __init__(self,Person,level):
+    def __init__(self,name='Jane Doe',age=30,gender='female',level='junior'):
+        
+        Person.__init__(self,name='Jane Doe',age=30,gender='female')
         self.level = level
     
     def get_goal(self):
         print("Educate brilliant junior software developers.")
     
-    def introduce(self,person):
-        print(f"Hi,I'm {person.name},a {person.age} year old {person.gender} {self.level} mentor.")
+    def introduce(self):
+        print(f"Hi,I'm {self.name},a {self.age} year old {self.gender} {self.level} mentor.")
 
-person = Person ('John Doe',30,'female')
-mentor = Mentor(Person,'junior')
+# person = Person ('John Doe',30,'female')
+# mentor = Mentor(Person,'junior')
 
-print(mentor.get_goal())
-print(mentor.introduce(person))
+# print(mentor.get_goal())
+# print(mentor.introduce(person))
