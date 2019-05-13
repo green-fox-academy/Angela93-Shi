7bf1b716cd4f514b933c3bb45b4e1e38792d5438
angela <Angela_Shi@epam.com>
2019-05-13 23:03:27 +0800

    update sponsor.py

--- week-02/day-7/inheritance/sponsor.py
+++ week-02/day-7/inheritance/sponsor.py
@@ -1,12 +1,13 @@
 from person import Person
 
 class Sponsor(Person):
-    def __init__(self,Person,company,hired_students):
+    def __init__(self,name='Jane Doe',age=30,gender='female',company='Google',hired_students=0):
+        Person.__init__(self,name='Jane Doe',age=30,gender='female')
         self.company = company
         self.hired_students = hired_students
     
-    def introduce(self,person):
-        print(f"Hi,I'm {person.name},a {person.age} year old {person.gender} who represents {self.company} and {self.hired_students} students so far")
+    def introduce(self):
+        print(f"Hi,I'm {self.name},a {self.age} year old {self.gender} who represents {self.company} and {self.hired_students} students so far")
     
     def hire(self):
         self.hired_students += 1
@@ -15,11 +16,11 @@ class Sponsor(Person):
     def get_goal(self):
         print("Hire brilliant junior software developers.")
 
-person = Person('Jane Doe',30,'female')
-sponsor = Sponsor(Person,'Googlr',0)
+# person = Person('Jane Doe',30,'female')
+# sponsor = Sponsor(Person,'Googlr',0)
 
-print(sponsor.introduce(person))
-print(sponsor.hire())
-print(sponsor.get_goal())
+# print(sponsor.introduce(person))
+# print(sponsor.hire())
+# print(sponsor.get_goal())
 
 
