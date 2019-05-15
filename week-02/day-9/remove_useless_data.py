c742e559a8f1fdd982fee9d4fed74ead1dc5d51a
angela <Angela_Shi@epam.com>
2019-05-15 21:03:11 +0800

    update f.close()

--- week-02/day-9/remove_useless_data.py
+++ week-02/day-9/remove_useless_data.py
@@ -3,6 +3,7 @@ import csv
 f=open('election.csv','r')
 csvreader = csv.reader(f)
 final_list=list(csvreader)
+f.close()
 
 for i in range(len(final_list)):
     if "" not in final_list[i] and len(final_list[i]) == 4:
