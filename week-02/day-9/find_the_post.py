bb8c0955958a19d13c999fb3e7924e4763741a4f
angela <Angela_Shi@epam.com>
2019-05-15 21:02:30 +0800

    update program

--- week-02/day-9/find_the_post.py
+++ week-02/day-9/find_the_post.py
@@ -5,16 +5,36 @@ with open(file_path, mode='r',encoding='utf-8') as f:
     print(type(js))
     print(len(js))
 
-like_count_list=[]
-for i in range(len(js)):
-    a=js[i]
-    like_count_list.append((a["like_count"]))
-print(like_count_list)
+# like_count_list=[]
+# for i in range(len(js)):
+#     a=js[i]
+#     like_count_list.append((a["like_count"]))
+# print(like_count_list)
+sum_list=[]
+for i in js:
+    if i["comments"] != None:
+        coments_list = i["comments"]
+        sum=0
+        for j in coments_list:
+            sum += j["like_count"]
+        sum_list.append(sum)
+print(sum_list)
 
-most_like=max(like_count_list)
-print(most_like)
+most_popular=0
+for i in sum_list:
+    if i > most_popular:
+        most_popular = i
+print(most_popular)
 
-for i in range(len(js)):
-    a=js[i]
-    if most_like == a["like_count"]:
-        print(js[i])
+for i in js:
+  if i["comments"] != None:
+        coments_list = i["comments"]
+        sum=0
+        for j in coments_list:
+            sum += j["like_count"]
+        if sum == most_popular:
+            print(i)
+
+
+
+
