24071a436480bfb78ad68300c3e916fc52db90d9
angela <Angela_Shi@epam.com>
2019-05-20 17:50:01 +0800

    add paperback_book.py

---
+++ week-03/day-01/paperback_book.py
@@ -0,0 +1,16 @@
+# It should have the following fields: title, author, release year, page number and weight.
+# The weight must be calculated from the number of pages (every page weighs 10 grams) plus the weight of the cover which is 20 grams.
+# It must have a method that returns a string which contains the following information about the book: author, title and year.
+from book import Book
+class Paperback_book(Book):
+    def __init__(self,author,title,release_year,page_number,weight):
+        Book.__init__(self,author,title,release_year)
+        self.page_number = page_number
+        self.weight = self.get_weight
+        
+    def toString(self):
+        return f'author: {self.author}, title: {self.title}, year: {self.release_year}'
+
+    def get_weight(self):
+        weight = self.page_number * 10 + 100 *2
+        return weight
