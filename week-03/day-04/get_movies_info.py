7a904b3186e8b42fc87cb0f9aeedd2ee09236847
angela <Angela_Shi@epam.com>
2019-05-23 15:54:27 +0800

    update get_movies_info.py

--- week-03/day-04/get_movies_info.py
+++ week-03/day-04/get_movies_info.py
@@ -7,6 +7,7 @@ from movies_info import movie_list
 
 app = Flask(__name__)
 
+# [GET] /api/movies
 @app.route('/api/movies')
 
 def api_root():
@@ -15,6 +16,75 @@ def api_root():
         movies.append(movie_dict)
     return jsonify(movies)
 
+# [GET] /api/movies/<movie_id>
+@app.route('/api/movies/<movie_id>')
+
+def get_selected_movie(movie_id):
+    movie_id=int(movie_id)
+    for movie_dict in movie_list:
+        if movie_dict['id'] == movie_id:
+            return jsonify(movie_dict)
+
+# [POST] /api/movies
+@app.route('/api/movies',methods=['POST'])
+def post_movie():
+    # key=request.headers['API_KEY']
+    if is_right_api():
+        try:
+            movie_id = request.form['id']
+            title = request.form['title']
+            year = request.form['year']
+            description = request.form['description']
+            movies=create_database(movie_id,title,year,description)
+            return jsonify(movies),200
+        except:
+            return "error"
+    else:
+        error_hint = {"error":"Invalid API_KEY"}
+        return jsonify(error_hint),403
+    
+
+def is_right_api():
+    API_KEY='1234'
+    key=request.headers['API_KEY']
+    if key == API_KEY:
+        return True
+
+def create_database(movie_id,title,year,description):
+    create_dict = {}
+    create_dict['id'] = movie_id
+    create_dict['title'] = title
+    create_dict['year'] = year
+    create_dict['description'] = description
+    
+    return create_dict
+
+# [PUT] /api/movies/<movie_id>
+
+
+@app.route('/api/movies/<movie_id>',methods=['PUT'])
+def put_movie(movie_id):
+    movie_get_id = request.form['id']
+    if movie_id == movie_get_id:
+        movie_replaced_id = "2"
+        movie_replaced_title = "The Kid Who Would Be King"
+        movie_replaced_year = "1990"
+        movie_replaced_description = "A band of kids embark on an epic quest to thwart a medieval menace."
+        update_dict = update_database(movie_replaced_id,movie_replaced_title,movie_replaced_year,movie_replaced_description)
+    if is_right_api():
+        return jsonify(update_dict),200
+    else:
+        return "error"
+ 
+
+def update_database(movie_replaced_id,movie_replaced_title,movie_replaced_year,movie_replaced_description):
+    update_dict = {}
+    update_dict['id'] = movie_replaced_id
+    update_dict['title'] = movie_replaced_title
+    update_dict['year'] = movie_replaced_year
+    update_dict['description'] = movie_replaced_description
+    
+    return update_dict
 
 
 if __name__ == "__main__":
