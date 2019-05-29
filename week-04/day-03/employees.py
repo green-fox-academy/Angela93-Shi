b1d939fea64c30a444307579e99115cc232c534c
angela <Angela_Shi@epam.com>
2019-05-29 21:03:07 +0800

    update employees.py

--- week-04/day-03/employees.py
+++ week-04/day-03/employees.py
@@ -2,6 +2,7 @@ import json
 import csv
 from xml.dom import minidom
 import psycopg2
+import datetime
 
 con=psycopg2.connect(
     host='localhost',
@@ -24,7 +25,7 @@ for json in jsondata:
     monthly_salary=json["monthly_salary"]
     birth_date = json["birth_date"]
     insert_query = 'INSERT INTO employees_table VALUES (%s,%s,%s,%s,%s,%s)'
-    # cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,birth_date))
+    cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,birth_date))
 con.commit()
 
 # transform the employees.csv into postgres
@@ -41,7 +42,7 @@ for employee in final_list:
     monthly_salary=employee[5]
     bitrh_date = employee[3]
     insert_query = 'INSERT INTO employees_table VALUES (%s,%s,%s,%s,%s,%s)'
-    # cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,bitrh_date))
+    cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,bitrh_date))
 con.commit()
 
 # transform the employees.xml into postgres
@@ -87,29 +88,59 @@ for i in range(len(ids)):
     monthly_salary = salary_by_month[i]
     bitrh_date = bitrh_date_list[i]
     insert_query ='INSERT INTO employees_table VALUES (%s,%s,%s,%s,%s,%s)'
-    # cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,bitrh_date))
+    cursor.execute(insert_query,(id,first_name,last_name,gender,monthly_salary,bitrh_date))
 
 con.commit()
 
 # Which first name is the most common in the company?
 def first_name_popular():
-    select_query='SELECT DISTINCT first_name,count(*) FROM employees_table GROUP BY first_name'
+    select_query='SELECT first_name,count(*) AS num FROM employees_table GROUP BY first_name ORDER BY num DESC LIMIT 1'
     cursor.execute(select_query)
     first_name_count = cursor.fetchall()
-    most_popular_first_name=0
-    for i in range(len(first_name_count)):
-        if first_name_count[i][1] > most_popular_first_name:
-            most_popular_first_name = first_name_count[i][1]
-    print(f'the most popular first name is {first_name_count[i][0]}')
-
-    
-
+    print(f'the most popular first name is {first_name_count[0][0]},appears {first_name_count[0][1]}')
 
 first_name_popular()
 
+# Which first name is the most common among the younger (<30) employees?
+def younger_first_name_popular():
+    select_query='SELECT first_name,count(*) AS num FROM employees_table WHERE (current_date - employees_table.birth_date)/365 < 30 GROUP BY first_name ORDER BY num DESC LIMIT 1'
+    cursor.execute(select_query)
+    first_name_count = cursor.fetchall()
+    print(f'the most popular first name is {first_name_count[0][0]},appears {first_name_count[0][1]}')
 
+younger_first_name_popular()
 
+# How many employee earns more than the average? 
+def employees_more_than_average():
+    select_query =''' SELECT count(id) FROM employees_table 
+                        WHERE monthly_salary > (
+                            SELECT AVG (monthly_salary) FROM employees_table
+                        )
+                    '''
+    cursor.execute(select_query)
+    people_count = cursor.fetchall()
+    print(f'there are {people_count[0][0]} people earns more that the average')
 
+employees_more_than_average()
+
+# Increase the salary by monthly $100 for everybody who earns less than the median
+def increase_money():
+    update_query=''' UPDATE employees_table SET monthly_salary = monthly_salary+100
+                    WHERE monthly_salary < (
+                        SELECT tab.monthly_salary FROM (
+                            SELECT a1.id,a1.monthly_salary,COUNT(a2.monthly_salary) salary_Rank
+                            FROM employees_table a1,employees_table a2
+                            WHERE a1.monthly_salary < a2.monthly_salary
+                            OR (a1.monthly_salary = a2.monthly_salary and a1.id =a2.id)
+                            GROUP BY a1.id,a1.monthly_salary
+                            ORDER BY a1.monthly_salary DESC
+                        ) as tab 
+                        WHERE tab.salary_Rank=(select count(*)/2 from employees_table)
+                    )
+                '''
+    cursor.execute(update_query)
+
+increase_money()
 
 
 select_query ='SELECT * FROM employees_table'
